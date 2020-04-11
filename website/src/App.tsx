59c197c7907fdbeac6ba1ce2c981d956e1840aaf
Stephen Davis <davi1138@purdue.edu>
2020-04-11 17:03:55 -0400

    added basic log statistic printing

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,10 +1,31 @@
 import React, { useEffect } from 'react';
 import './App.css';
 
+type Props = {
+  value: number;
+  valueColor?: string;
+  description: string;
+}
+
+function DataCard({ value, valueColor, description }: Props) {
+  return (
+    <div className='App-card'>
+      <div style={{ fontSize: '2em', textAlign: 'center' }}>
+        <h1 style={{ color: valueColor!= undefined ? valueColor : '#FF2000', fontWeight: 800, fontSize: '3em', marginBottom: 0 }}>{value}</h1>
+        <h1>{description}</h1>
+      </div>
+    </div>
+  );
+}
+
+let addresses: Set<string> = new Set();
+
 function App() {
   const [url, setUrl] = React.useState('');
-  const [response, setResponse] = React.useState('');
   const [button, setButton] = React.useState(false)
+  const [numberOfAcceptedLogins, setNumberOfAcceptedLogins] = React.useState(0);
+  const [numberOfFailedLogins, setNumberOfFailedLogins] = React.useState(0);
+  const [numberOfPasswordChangeAttempts, setNumberOfPasswordChangeAttempts] = React.useState(0);
 
   useEffect(() => {
     let handle = setInterval(() => {
@@ -16,16 +37,30 @@ function App() {
     }
   }, [button])
 
-  function handleButton( ) {
+  function handleButton() {
     setButton(!button);
+    addresses.clear()
     getLogs()
   }
 
   function getLogs() {
-    console.log(url)
     fetch(url, { method: 'GET', mode: 'cors' }).then(response => response.text()).then(responseText => {
-      let output = responseText.split('\n').filter(item => item.includes('password for pi'))
-      setResponse(output.join('\n'))
+      let acceptedPasswordAttempts = responseText.split('\n').filter(item => item.includes('Accepted password for pi'))
+      setNumberOfAcceptedLogins(acceptedPasswordAttempts.length);
+
+      let failedPasswordAttempts = responseText.split('\n').filter(item => item.includes('Failed password for pi'))
+      setNumberOfFailedLogins(failedPasswordAttempts.length);
+
+      let accessAttempts = responseText.split('\n').filter(item => item.includes('password for pi'))
+      for (let lineInfo in accessAttempts) {
+        let attackerAddress = new RegExp('((::[\d|\w]+:[\d|\w]+:[\d|\w]+:[\d|\w]+)|(\d+\.\d+\.\d+\.\d+))').exec(lineInfo)
+        if (attackerAddress != null) {
+          addresses.add(attackerAddress[0]);
+        }
+      }
+
+      let passwordChangeAttempts = responseText.split('\n').filter(item => item.includes('password changed for pi'))
+      setNumberOfPasswordChangeAttempts(passwordChangeAttempts.length);
     });
   }
 
@@ -38,8 +73,11 @@ function App() {
         <input id="url-input" className='App-input' type='text' placeholder={'enter the url of honeypot'} onChange={(event) => setUrl(event.target.value)}></input>
         <button className='App-button' onClick={e => handleButton()}>get statistics</button>
       </section>
-      <section>
-        <pre>{response}</pre>
+      <section className='App-grid' style={{ marginTop: 64 }}>
+        <DataCard value={numberOfFailedLogins} description={'successful access attempts from external users'} />
+        <DataCard value={numberOfAcceptedLogins} description={'failed access attempts from external users'} />
+        <DataCard value={addresses.size} description={'unique external users'} />
+        <DataCard value={numberOfPasswordChangeAttempts} description={'password change attempts from external users'} />
       </section>
     </div>
   );
