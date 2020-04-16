6a9daa1cc8a48852ea4db2a54120ed984e3d857b
Stephen Davis <davi1138@purdue.edu>
2020-04-16 15:34:25 -0400

    Updates

---
+++ precache-manifest.c9a0cb3b5ffcc2e67b36fa8095ce131f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67ce9a369bf2c6c0070850b6ad9f6435",
+    "url": "/CS422-PIoT/website/index.html"
+  },
+  {
+    "revision": "eb18469c0a2b0cf4cf2d",
+    "url": "/CS422-PIoT/website/static/css/main.9738bf5c.chunk.css"
+  },
+  {
+    "revision": "0a42f3d677dad40566f2",
+    "url": "/CS422-PIoT/website/static/js/2.6e475bce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CS422-PIoT/website/static/js/2.6e475bce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb18469c0a2b0cf4cf2d",
+    "url": "/CS422-PIoT/website/static/js/main.7134cfb7.chunk.js"
+  },
+  {
+    "revision": "053ef0198b93bdc03887",
+    "url": "/CS422-PIoT/website/static/js/runtime-main.f12d2de7.js"
+  }
+]);
